31e16f397a0f963001a2309b49b75a9f29aa8a48
Danilo Almeida <cs.daniloalmeida@gmail.com>
2022-12-12 14:27:45 -0300

    added options for width,height,distance,size

--- spot-painting-generator.py
+++ spot-painting-generator.py
@@ -8,7 +8,12 @@ import colorgram
 import random
 import os
 
+# Configuring parser
 parser = argparse.ArgumentParser('Generate Hirst\'s Spot Paintings')
+parser.add_argument('-w', '--width', metavar='', type=int, required=False, help='number of dots in width')
+parser.add_argument('-H', '--height', metavar='', type=int, required=False, help='number of dots in height')
+parser.add_argument('-s', '--size', metavar='', type=int, required=False, help='dot size (default 20)')
+parser.add_argument('-d', '--distance', metavar='', type=int, required=False, help='distance between each dot (default 50)')
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-r', '--random', action='store_true', help='random colors')
 args = parser.parse_args()
@@ -49,21 +54,26 @@ turtle.penup()
 turtle.speed(0)
 turtle.hideturtle()
 
-dotsize = 20
-dots_per_row = 10
-space_between_dots = 50
+dotsize = args.size if args.size else 20
+space_between_dots = args.distance if args.distance else 50
+dots_height = args.height if args.height else 10
+dots_width = args.width if args.width else 10
 
 screen = Screen()
-sc = dots_per_row * space_between_dots + 50
-screen.setup(sc, sc)
+h = dots_height * space_between_dots + space_between_dots
+w = dots_width * space_between_dots + space_between_dots
+screen.setup(w, h)
+screen.screensize(w - 2, h - 2)
 screen.colormode(255)
 
-pos = (dots_per_row - 1) * space_between_dots / 2
-turtle.setx(-pos)
-turtle.sety(-pos)
+xpos = (dots_width - 1) * space_between_dots / 2
+turtle.setx(-xpos)
+
+ypos = (dots_height - 1) * space_between_dots / 2
+turtle.sety(-ypos)
 
 drawn_in_row = 0
-for _ in range(dots_per_row**2):
+for _ in range(dots_height * dots_width):
     if args.random:
         color = tuple(random.randint(0,255) for _ in range(3))
     else:
@@ -71,9 +81,9 @@ for _ in range(dots_per_row**2):
     turtle.dot(dotsize, color)
     turtle.forward(space_between_dots)
     drawn_in_row += 1
-    if drawn_in_row >= dots_per_row:
+    if drawn_in_row >= dots_width:
         drawn_in_row = 0
-        turtle.setx(-pos)
+        turtle.setx(-xpos)
         turtle.sety(turtle.ycor() + space_between_dots)
 
 if not os.path.exists('results/'):
